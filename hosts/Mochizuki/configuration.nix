bd96bb3bcd905dea9253b9042c4e56bcb8621fe3
YotsuhaMeru <79040168+YotsuhaMeru@users.noreply.github.com>
2023-10-17 18:32:32 +0000

    Add Mochizuki conf

---
+++ hosts/Mochizuki/configuration.nix
@@ -0,0 +1,55 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostName = "Folkroll"; # Define your hostname.
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "jp";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "jp106";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.kaguya = {
+    isNormalUser = true;
+    description = "Momose Kaguya";
+    extraGroups = [ "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [ ];
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.enable = true;
+
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
