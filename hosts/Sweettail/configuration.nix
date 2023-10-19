468b4e0b31fbe3f700b874a4361df593466845b8
YotsuhaMeru <79040168+YotsuhaMeru@users.noreply.github.com>
2023-10-19 20:35:37 +0900

    fix

--- hosts/Sweettail/configuration.nix
+++ hosts/Sweettail/configuration.nix
@@ -7,15 +7,7 @@
   ...
 }: {
   networking.hostName = "Sweettail";
-  # Set your time zone.
-  time.timeZone = "Asia/Tokyo";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "jp106";
