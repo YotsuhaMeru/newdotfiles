f1ed813750a2664a6c4f0aa2fc312639b11a182e
YotsuhaMeru <79040168+YotsuhaMeru@users.noreply.github.com>
2024-05-04 04:33:18 +0900

    update

--- hosts/YurigamineGakuen/default.nix
+++ hosts/YurigamineGakuen/default.nix
@@ -24,6 +24,20 @@
   hardware.enableAllFirmware = true;
   hardware.enableRedistributableFirmware = true;
 
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "i965"; }; # Force intel-media-driver
+
   networking.hostName = "YurigamineGakuen";
   networking.useDHCP = lib.mkDefault true;
   networking.networkmanager = {
@@ -34,7 +48,7 @@
     };
   };
   networking.firewall.enable = true;
-  
+   
   services.openssh = {
     enable = true;
     settings.PermitRootLogin = "no";
